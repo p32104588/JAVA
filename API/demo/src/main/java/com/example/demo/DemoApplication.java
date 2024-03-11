5c04bee89ff7650a7b7f8da3fc545c32fd5efaa0
CHAOCHIAHSUAN-N\ChaoChiaHsuan <>
2024-03-11 15:55:08 +0800

    JAVA API update to be use

--- API/demo/src/main/java/com/example/demo/DemoApplication.java
+++ API/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,7 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-import demo.ConnString;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -15,4 +9,5 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
